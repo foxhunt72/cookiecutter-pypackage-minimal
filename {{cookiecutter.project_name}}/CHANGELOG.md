6f5e78306c5c9f182f7fea7944e57e439c26826c
Richard de Vos <richard.de.vos@sentia.com>
2022-05-02 16:27:01 +0200

    small fixes, bumversion support etc

---
+++ {{cookiecutter.project_name}}/CHANGELOG.md
@@ -0,0 +1,8 @@
+## Changelog
+
+{{ cookiecutter.author_name }} <{{ cookiecutter.author_email }}>
+{{ cookiecutter.package_name }}
+{{ cookiecutter.project_name }}
+
+--------------------------------------------------------------
+{{ cookiecutter.package_version }}: initial creation
--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__version__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__version__.py
@@ -4,4 +4,6 @@ __version__ = "{{ cookiecutter.package_version }}"
 __author__ = "{{ cookiecutter.author_name }} <{{ cookiecutter.author_email }}>"
 __author_name__ = "{{ cookiecutter.author_name }}"
 __author_email__ = "{{ cookiecutter.author_email }}"
+__app_name__ = "{{ cookiecutter.package_name }}"
+__project_name__ = "{{ cookiecutter.project_name }}"
 __all__ = []
